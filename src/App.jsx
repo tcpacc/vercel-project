3a3f8242538220fc7a5ab16b440fb56740fab081
tcpacc <sepbepp@gmail.com>
2025-08-02 20:46:27 +0700

    trying to use react router dom in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import { useState } from 'react'
 import { GlobalContext } from '../context'
-import { RouterProvider } from 'react-router-dom'
-import { router } from '../routes'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+// import { router } from '../routes'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from '../pages/home'
+import About from '../pages/about'
+import Contact from '../pages/contact'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
      <>
-    <RouterProvider router={router}/>
-
+    {/* <RouterProvider router={router}/> */}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact/>} />
+      </Routes>
+    </Router>
     </>
   )
 }
